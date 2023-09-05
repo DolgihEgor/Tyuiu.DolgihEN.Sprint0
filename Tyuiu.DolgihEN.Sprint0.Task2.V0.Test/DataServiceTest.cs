912e6d9b95fedbf04744aa3fb1eac374dee80a86
Tyuiu.DolgihEN@outlook.com <egordolgih02@list.ru>
2023-09-05 10:43:09 +0500

    Task2

---
+++ Tyuiu.DolgihEN.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DolgihEN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.DolgihEN.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов, тестирования, методов из библиотеки 
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            //Вызывает класс Assert
+            Assert.AreEqual("Привет..., Егор", res);
+        }
+    }
+}
