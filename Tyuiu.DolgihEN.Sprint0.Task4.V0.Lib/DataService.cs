e9c9768f165c924468afb6288d2c7753caf6497c
Tyuiu.DolgihEN@outlook.com <egordolgih02@list.ru>
2023-09-05 11:28:45 +0500

    Task4

---
+++ Tyuiu.DolgihEN.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.DolgihEN.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+       //Пример лин. структуры
+       // Сложение
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Вычитание 
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Умножение
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        //Деление
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Tyuiu.DolgihEN.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DolgihEN.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DolgihEN.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71d45c4b-66bb-4cf6-bede-ef1f6c656fba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
